22989d3540a90225ec43ba198c37c8db2ab1351a
Emilien Duperthuy <emilien@snagr.com.hk>
2021-08-16 13:55:05 +0200

    Adding Xamarin.Forms testing projects

---
+++ Working_xamarinforms_example_ReactiveUI_14.1.1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Xamarin.Forms;
+
+namespace Working_xamarinforms_example_ReactiveUI_14_1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var command = ReactiveCommand.Create<string>(id => {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    this.Label.Text = $"Command executed, execution ID: {id}";
+                });
+            });
+
+
+            // NOTE: This code works just fine
+
+            //Observable.FromEventPattern(
+            //    h => this.Button.Clicked += h,
+            //    h => this.Button.Clicked -= h)
+            //    .Select(_ => Guid.NewGuid().ToString())
+            //    .InvokeCommand(command);
+
+            // This code works
+            Observable.FromEventPattern(
+                h => this.Button.Clicked += h,
+                h => this.Button.Clicked -= h)
+                .Subscribe(_ => {
+                    Observable.Return(Guid.NewGuid().ToString())
+                              .InvokeCommand(command);
+
+                });
+        }
+    }
+}
